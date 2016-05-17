3e31a1718c6418ffcda8bfa283fc357233b75412
Jeroen Hoogers <hoogers.jeroen@gmail.com>
2016-05-17 13:13:44 +0200

    Initial commit

---
+++ assignment1/TinkerToy/TinkerToy/GLUtil.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// OpenGL Includes
+#include <gl/glew.h> //Needs to be included first
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <gl/glext.h>
+#include <gl/wglew.h>
+
+#include <gl/freeglut.h>
+
+// Link to libraries
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "SOIL.lib")
+#pragma comment(lib, "freeglut.lib")
