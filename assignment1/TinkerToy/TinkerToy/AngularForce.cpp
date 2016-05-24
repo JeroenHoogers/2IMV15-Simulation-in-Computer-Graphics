d94c3dbc81db14c22ae09efd55b556b036d851d7
Niek <niek_vanhulzen@hotmail.com>
2016-05-24 14:42:43 +0200

    Cloth and Hair updated, also added enablings

--- assignment1/TinkerToy/TinkerToy/AngularForce.cpp
+++ assignment1/TinkerToy/TinkerToy/AngularForce.cpp
@@ -7,29 +7,6 @@ AngularForce::AngularForce(Particle *p1, Particle * p2, Particle * p3, double an
 
 void AngularForce::draw()
 {
-	Vec2f positionDiff = m_p1->m_Position - m_p2->m_Position;
-	float distance = sqrt(positionDiff[0] * positionDiff[0] + positionDiff[1] * positionDiff[1]);
-	float stretch = (distance / m_angle) - 1;
-
-	// Color changes as the spring distance changes
-	// blue = rest distance
-	// red = > rest distance
-
-	glBegin(GL_LINES);
-	glColor3f(0.5 + stretch, 0.3, 1 - stretch * 2);
-	glVertex2f(m_p1->m_Position[0], m_p1->m_Position[1]);
-	glColor3f(0.5 + stretch, 0.3, 1 - stretch * 2);
-	glVertex2f(m_p2->m_Position[0], m_p2->m_Position[1]);
-
-	positionDiff = m_p2->m_Position - m_p3->m_Position;
-	distance = sqrt(positionDiff[0] * positionDiff[0] + positionDiff[1] * positionDiff[1]);
-	stretch = (distance / m_angle) - 1;
-
-	glColor3f(0.5 + stretch, 0.3, 1 - stretch * 2);
-	glVertex2f(m_p2->m_Position[0], m_p2->m_Position[1]);
-	glColor3f(0.5 + stretch, 0.3, 1 - stretch * 2);
-	glVertex2f(m_p3->m_Position[0], m_p3->m_Position[1]);
-	glEnd();
 
 }
 
@@ -43,10 +20,12 @@ void AngularForce::apply()
 	Vec2f velocityDiff1to2 = m_p1->m_Velocity - m_p2->m_Velocity;
 	Vec2f velocityDiff3to2 = m_p3->m_Velocity - m_p2->m_Velocity;
 
+	// calculate distance to ther middle vertex
 	float distance1to2 = sqrt(positionDiff1to2[0] * positionDiff1to2[0] + positionDiff1to2[1] * positionDiff1to2[1]);
 	float distance3to2 = sqrt(positionDiff3to2[0] * positionDiff3to2[0] + positionDiff3to2[1] * positionDiff3to2[1]);
 	float distance1to2Sqrt = positionDiff1to2[0] * positionDiff1to2[0] + positionDiff1to2[1] * positionDiff1to2[1];
 	float distance3to2Sqrt = positionDiff3to2[0] * positionDiff3to2[0] + positionDiff3to2[1] * positionDiff3to2[1];
+
 
 	float distanceDot = (positionDiff1to2[0] * positionDiff3to2[0] + positionDiff1to2[1] * positionDiff3to2[1]);
 
@@ -55,26 +34,15 @@ void AngularForce::apply()
 	float cosineSqrt = distanceDot * distanceDot / distance1to2Sqrt / distance3to2Sqrt;
 	float cosine = 2 * cosineSqrt - 1;
 
-	/*float arccosDist;
-	if (distanceTotal > -1.0 && distanceTotal < 1.0)
-		arccosDist = acos(distanceTotal);
-	else if (distanceTotal >= 1.0)
-		arccosDist = 0;
-	else if (distanceTotal <= -1.0)
-		arccosDist = pi;*/
+	float alphasqr;
+	if (cosine <= -1) 
+		alphasqr = pi;
+	else if (cosine >= 1) 
+		alphasqr = 0;
+	else 
+		alphasqr = acosf(cosine);
 
-	/*float angle;
-	float D = (positionDiff1to2[0] * positionDiff3to2[1] - positionDiff3to2[0] * positionDiff1to2[1]);
-	if (D < 0)
-		angle = m_angle - arccosDist;
-	else
-		angle = arccosDist - m_angle;*/
-	float alpha2 =
-		(cosine <= -1) ? pi :
-		(cosine >= 1) ? 0 :
-		acosf(cosine);
-
-	float result = alpha2 / 2;
+	float result = alphasqr / 2;
 	result = result * 180. / pi;
 
 	if (distanceDot < 0)
@@ -95,11 +63,13 @@ void AngularForce::apply()
 	{
 		angle = result - m_angle;
 	}
-	float radangle = 0.001f*(angle * pi / 180);
+	
+	//scale the force so it doesn't apply to much force
+	angle = 0.001f*angle;
 
-	m_p1->m_Force[0] -= ((m_ks * radangle) / distance1to2 * positionDiff1to2[1] ) + (m_kd * velocityDiff1to2[0]);
-	m_p1->m_Force[1] += ((m_ks * radangle) / distance1to2 * positionDiff1to2[0]) - (m_kd * velocityDiff1to2[1]);
+	m_p1->m_Force[0] -= ((m_ks * angle) / distance1to2 * positionDiff1to2[1]) + (m_kd * velocityDiff1to2[0]);
+	m_p1->m_Force[1] += ((m_ks * angle) / distance1to2 * positionDiff1to2[0]) - (m_kd * velocityDiff1to2[1]);
 
-	m_p3->m_Force[0] += ((m_ks * radangle) / distance3to2 * positionDiff3to2[1]) - (m_kd * velocityDiff3to2[0]);
-	m_p3->m_Force[1] -= ((m_ks * radangle) / distance3to2 * positionDiff3to2[0]) + (m_kd * velocityDiff3to2[1]);
+	m_p3->m_Force[0] += ((m_ks * angle) / distance3to2 * positionDiff3to2[1]) - (m_kd * velocityDiff3to2[0]);
+	m_p3->m_Force[1] -= ((m_ks * angle) / distance3to2 * positionDiff3to2[0]) + (m_kd * velocityDiff3to2[1]);
 }
